fa028b557e20d01d933f7fe42a8f47ab20d8cc5a
Rahul Seetharaman <rahulseetharaman@gmail.com>
2018-05-31 18:51:54 +0530

    Update runfs.sh

--- runfs.sh
+++ runfs.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 keyczart create --location=/tmp/kz --purpose=crypt 
 keyczart addkey --location=/tmp/kz --status=primary
+#specify source and mountpoint inplace of the two path locations in the following line
 python3 fusepy.py /home/rahulseetharaman/Documents/fs /home/rahulseetharaman/Documents/mntpoint
